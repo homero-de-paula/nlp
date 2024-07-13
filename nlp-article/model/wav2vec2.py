eb2accb75e1fcccb4408fa3e328af573619ff39f
homero-de-paula <129883380+homero-de-paula@users.noreply.github.com>
2024-07-12 22:50:58 -0300

    Delete nlp-article/model directory

--- nlp-article/model/wav2vec2.py
+++
@@ -1,4 +0,0 @@
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
-
-processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
