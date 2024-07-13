616754b083f453e7c48471885b8dacf8efc5758d
homero-de-paula <129883380+homero-de-paula@users.noreply.github.com>
2024-07-12 23:03:24 -0300

    Update clear_dataset.py

--- nlp-article/clear_dataset.py
+++ nlp-article/clear_dataset.py
@@ -6,8 +6,8 @@ from asr_language_model_evaluation.preprocessing import normalize
 
 datasets_type = {
     'coraa': Coraa,
-    'cetuc': cetuc,
-    'commonvoice': commonvoice
+    'cetuc': Cetuc,
+    'commonvoice': CommonVoice
 }
 
 if __name__ == '__main__':
